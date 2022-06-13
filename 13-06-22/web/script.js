3756f35ce852f3170b0b97a074fb17daa1244a3e
Sam Bradbury <sam.bradbury@hotmail.com>
2022-06-13 19:26:01 +1000

    more array work

--- 13-06-22/web/script.js
+++ 13-06-22/web/script.js
@@ -1,17 +1,60 @@
-let myArray = ["A", "B", "C"];
+const rainbowColours = [];
 
-console.log(myArray[0]);
-console.log(myArray[2]);
-console.log(myArray[3]);
+// push to end of array
+rainbowColours.push("orange");
+// insert into the start of the array
+rainbowColours.unshift("red");
+// push to end of array
+rainbowColours.push("yellow");
+// push multiple items to end of array in the order entered
+rainbowColours.push("green", "blue", "indigo", "violet");
+console.log(rainbowColours);
+console.log(`length of rainbowColours is ${rainbowColours.length}`);
+console.log(rainbowColours[1]);
+console.log(`last item in rainbowColours is ${rainbowColours[rainbowColours.length - 1]}`);
+console.log(`index of "blue" is ${rainbowColours.indexOf("blue")}`);
+// creates new array of items start 1 and end 3.
+// the end stops collecting and doesnt include that item.
+// items 1 & 2 are collected into a new array, not 3
+console.log(`slice 1 and 3 is ${rainbowColours.slice(1,3)}`);
+// removes items 1, 2 & 3
+console.log(`splice 1 and 3 is ${rainbowColours.splice(1,3)}`);
+console.log(rainbowColours);
 
-// add array at the end
-myArray.push("D");
-myArray.push("E");
+const twoColors = rainbowColours.slice(1,3);
+console.log(twoColors);
 
-console.log(myArray);
+const nums = [0, 1, 2, 2, 2, 3, 3, 4, 5];
+console.log(nums);
+nums.splice(3,3);
+console.log(nums);
 
-//remove item at end of array
-myArray.pop();
+const arrOfArrs = [["inner array first item", "inner array second item"], ["first", "second", "third"]];
 
-console.log(myArray);
+console.log(arrOfArrs[0][0]);
+console.log(arrOfArrs[arrOfArrs.length - 1][arrOfArrs[arrOfArrs.length - 1].length - 1]);
 
+for (let i = 0; i < arrOfArrs[arrOfArrs.length - 1].length; i++){
+    console.log(arrOfArrs[1][i]);
+}
+
+const topFive = ["Bender", "Fry", "Zoidburg", "Professor", "Zapp Brannigan"];
+
+for (let i = 0; i < topFive.length; i++){
+    let prefix = '';
+    switch (i){
+        case 0:
+            prefix = 'st';
+            break;
+        case 1:
+            prefix = 'nd';
+            break;
+        case 2:
+            prefix = 'rd';
+            break;
+        default:
+            prefix = 'th';
+            break;
+    }
+    console.log(`my ${i + 1}${prefix} choice is ${topFive[i]}!`);
+}
