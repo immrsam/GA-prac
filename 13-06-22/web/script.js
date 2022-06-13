41c652239dc617cb606f279603ede5d2d79a8b88
Sam Bradbury <sam.bradbury@hotmail.com>
2022-06-13 18:38:30 +1000

    array work

--- 13-06-22/web/script.js
+++ 13-06-22/web/script.js
@@ -1,1 +1,17 @@
-console.log("loaded");
+let myArray = ["A", "B", "C"];
+
+console.log(myArray[0]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+
+// add array at the end
+myArray.push("D");
+myArray.push("E");
+
+console.log(myArray);
+
+//remove item at end of array
+myArray.pop();
+
+console.log(myArray);
+
