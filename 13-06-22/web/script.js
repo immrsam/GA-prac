bea929d6f02e252b06d59a2f4a38412d84bad7f6
Sam Bradbury <sam.bradbury@hotmail.com>
2022-06-14 22:59:37 +1000

    functional programming prac

--- 13-06-22/web/script.js
+++ 13-06-22/web/script.js
@@ -111,6 +111,10 @@ const arrOfBooks = [
         alreadyRead: true
     }
 ];
+console.log("------------------");
+arrOfBooks.map((b) => console.log(b.title));
+arrOfBooks.forEach((b) => console.log(b.title));
+console.log("------------------");
 
 // console.log(arrOfBooks);
 
@@ -178,3 +182,132 @@ recepieIngredients.appendChild(recepieIngredientsList);
 container.appendChild(recepieHeading);
 container.appendChild(recepieServes);
 container.appendChild(recepieIngredients);
+
+
+function videoPairs() {
+	let newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	],
+	videoAndTitlePairs = [];
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Use forEach function to accumulate {id, title} pairs from each video.
+	// Put the results into the videoAndTitlePairs array using the Array's
+	// push() method. Example: videoAndTitlePairs.push(newItem);
+	// ------------ INSERT CODE HERE! -----------------------------------
+    newReleases.forEach((e) => videoAndTitlePairs.push({id: e.id, title: e.title}));
+    // newReleases.forEach((e) => console.log(e.id));
+    // newReleases.forEach(function(video) {
+    //     videoAndTitlePairs.push({ id: video.id, title: video.title });
+    // });
+
+	return videoAndTitlePairs;
+}
+
+console.log(videoPairs());
+
+Array.prototype.map = function(projectionFunction) {
+	let results = [];
+	this.forEach(function(itemInArray) {
+        results.push(projectionFunction(itemInArray));
+
+
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Apply the projectionFunction to each item in the array and add
+		// each result to the results array.
+		// Note: you can add items to an array with the push() method.
+		// ------------ INSERT CODE HERE! ----------------------------
+    });
+
+	return results;
+};
+
+// JSON.stringify([1,2,3].map(function(x) { return x + 1; })) === '[2,3,4]'
+
+
+// Array.prototype.map = function(projectionFunction) {
+// 	var results = [];
+// 	this.forEach(function(itemInArray) {
+// 		results.push(projectionFunction(itemInArray));
+
+// 	});
+
+// 	return results;
+// };
+
+// // JSON.stringify([1,2,3].map(function(x) { return x + 1; })) === '[2,3,4]'
+
+function newReleaseMap() {
+	var newReleases = [
+			{
+				"id": 70111470,
+				"title": "Die Hard",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [4.0],
+				"bookmark": []
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [5.0],
+				"bookmark": [{ id: 432534, time: 65876586 }]
+			},
+			{
+				"id": 65432445,
+				"title": "The Chamber",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [4.0],
+				"bookmark": []
+			},
+			{
+				"id": 675465,
+				"title": "Fracture",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [5.0],
+				"bookmark": [{ id: 432534, time: 65876586 }]
+			}
+		];
+
+  return newReleases.map(
+    function(video)
+    {
+        return { id: video.id, title: video.title };
+    });
+}
+
