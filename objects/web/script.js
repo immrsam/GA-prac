432f1230fb2dfc0270bad84abcd0df7f2e0af97f
s3768719 <s3768719@student.rmit.edu.au>
2022-06-18 15:17:14 +1000

    closing prac

--- objects/web/script.js
+++ objects/web/script.js
@@ -1,17 +1,8 @@
 const container = document.getElementById("container");
 
-const people = [
-    {
-        firstName: "John",
-        lastName : "Doe",
-        id       : 5566,
-        fullName : function() {
-            return this.firstName + " " + this.lastName;
-        }
-  }
-];
+const people = [];
 
-const addPerson = (objectArr, fName, lName, newId) => {
+const addPerson = (objectArr, fName, lName, userId, isAdmin = false) => {
     
     if(typeof objectArr !== 'object'){
         console.log("Not an object");
@@ -25,18 +16,30 @@ const addPerson = (objectArr, fName, lName, newId) => {
         console.log("Last name needs to be string");
         return false;
     }
-    if(typeof newId !== 'number'){
+    if(typeof userId !== 'number'){
         console.log("ID needs to be number");
         return false;
     }
+    if(typeof isAdmin !== 'boolean'){
+        console.log("Admin check needs to be true or false");
+        return false;
+    }
+
+    console.log(`Attempting to add ${fName} ${lName} ${userId}`);
+    
+    if(!isIdAvailable(objectArr, userId)){
+        console.log(`${userId} already exists`);
+        return false;
+    };
 
     try{
         const person = {
             firstName: fName,
             lastName : lName,
-            id       : newId,
+            id       : userId,
+            admin    : isAdmin,
             fullName : function() {
-                return this.firstName + " " + this.lastName;
+                return this.id + " " + this.firstName + " " + this.lastName + " Admin: " + this.admin;
             }
         }
         objectArr.push(person);
@@ -48,16 +51,20 @@ const addPerson = (objectArr, fName, lName, newId) => {
     return true;
 }
 
-addPerson(people, "Sam", "Bradbury", 3434);
+const isIdAvailable = (objectArr, userId) => {
+    let userSearch = objectArr.find(item => item.id === userId);
+    if(userSearch != null){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+addPerson(people, "Sam", "Bradbury", 3434, true);
 addPerson(people, "This", "Guy", 3214);
 addPerson(people, "Bud", "Smith", 6678);
 
-addPerson("people", 111, 222, "6678");
-addPerson(people, 111, 222, "6678");
-addPerson(people, "111", 222, "6678");
-addPerson(people, "111", "222", "6678");
-addPerson(people, "first", "last", 6678);
-
+addPerson(people, "Nobby", "Blinky", 6678);
 
 people.forEach(p => {
     console.log(p.fullName());
