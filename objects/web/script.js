4f80f9b06d3d37365676e54c4ba98a32a8ad7303
s3768719 <s3768719@student.rmit.edu.au>
2022-06-18 11:17:27 +1000

    updated bp

--- objects/web/script.js
+++ objects/web/script.js
@@ -1,6 +1,4 @@
 const container = document.getElementById("container");
-const heading = document.createElement("h1");
 
-heading.innerText = "Heading";
+console.log("loaded");
 
-container.appendChild(heading);
