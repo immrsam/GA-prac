6a5b0fc139b910d65e6ecbc60cc03b439a627bcf
Sam Bradbury <sam.bradbury@hotmail.com>
2022-06-19 21:29:50 +1000

    messing around with arrays

--- 20-06-22/web/script.js
+++ 20-06-22/web/script.js
@@ -1,1 +1,46 @@
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
+
+const topFive = ["Bender", "Fry", "Zoidburg", "Professor", "Zapp Brannigan"];
+
+for (let i = 0; i < topFive.length; i++){
+    let prefix = '';
+    switch (i){
+        case 0:
+            prefix = 'st';
+            break;
+        case 1:
+            prefix = 'nd';
+            break;
+        case 2:
+            prefix = 'rd';
+            break;
+        default:
+            prefix = 'th';
+            break;
+    }
+    console.log(`my ${i + 1}${prefix} choice is ${topFive[i]}!`);
+}
+
+const topFiveColour = ["Blue", "Red", "Orange", "Green", "Yellow"];
+
+topFiveColour.forEach(item => {
+    let prefix = '';
+    let position = topFiveColour.indexOf(item) + 1;
+
+    switch (position){
+        case 1:
+            prefix = 'st';
+            break;
+        case 2:
+            prefix = 'nd';
+            break;
+        case 3:
+            prefix = 'rd';
+            break;
+        default:
+            prefix = 'th';
+            break;
+    }
+    console.log(`My ${position}${prefix} choice is ${item}`);
+})
+
