20a65ef667496ad9e8d14aeed5267c50790f965e
Sam Bradbury <sam.bradbury@hotmail.com>
2022-06-19 21:40:50 +1000

    exercises and moved code around

--- 13-06-22/web/script.js
+++ 13-06-22/web/script.js
@@ -59,6 +59,29 @@
 //     console.log(`my ${i + 1}${prefix} choice is ${topFive[i]}!`);
 // }
 
+// const topFiveColour = ["Blue", "Red", "Orange", "Green", "Yellow"];
+
+// topFiveColour.forEach(item => {
+//     let prefix = '';
+//     let position = topFiveColour.indexOf(item) + 1;
+
+//     switch (position){
+//         case 1:
+//             prefix = 'st';
+//             break;
+//         case 2:
+//             prefix = 'nd';
+//             break;
+//         case 3:
+//             prefix = 'rd';
+//             break;
+//         default:
+//             prefix = 'th';
+//             break;
+//     }
+//     console.log(`My ${position}${prefix} choice is ${item}`);
+// })
+
 // let count = 0;
 
 // topFive.forEach((item) => {
--- 20-06-22/web/script.js
+++ 20-06-22/web/script.js
@@ -1,46 +1,67 @@
-// const container = document.getElementById("container");
+const container = document.getElementById("container");
 
-const topFive = ["Bender", "Fry", "Zoidburg", "Professor", "Zapp Brannigan"];
+// Scope, closures
 
-for (let i = 0; i < topFive.length; i++){
-    let prefix = '';
-    switch (i){
-        case 0:
-            prefix = 'st';
-            break;
-        case 1:
-            prefix = 'nd';
-            break;
-        case 2:
-            prefix = 'rd';
-            break;
-        default:
-            prefix = 'th';
-            break;
-    }
-    console.log(`my ${i + 1}${prefix} choice is ${topFive[i]}!`);
+// `Scope` is the set of variables, objects, and functions you have access to.
+
+// JavaScript has two scopes: `global` and `local`. A variable that is declared outside a function definition is a
+// `global` variable, and its value is accessible and modifiable throughout your program. A variable that is declared
+// inside a function definition is `local`. It is created and destroyed every time the function is executed, and it
+// cannot be accessed by any code outside the function.
+
+// Functions defined inside other functions, known as nested functions or closures, have access to their parent function's scope.
+
+// Pay attention to the comments in the code below.
+// Before running the code think what would be the output of `foo()`
+
+const aa = 4 // a is a global variable, it can be accessed by the functions below
+
+function foo () {
+  const bb = aa * 3 // b cannot be accessed outside foo function, but can be accessed by functions
+  // defined inside foo
+  function bar (cc) {
+    const bb = 2 // another `b` variable is created inside bar function scope
+    // the changes to this new `b` variable don't affect the old `b` variable
+    console.log(aa, bb, cc)
+  }
+
+  bar(bb * 4)
 }
 
-const topFiveColour = ["Blue", "Red", "Orange", "Green", "Yellow"];
+foo()
 
-topFiveColour.forEach(item => {
-    let prefix = '';
-    let position = topFiveColour.indexOf(item) + 1;
+// I.I.F.E., Immediately Invoked Function Expression, is a common pattern for creating local scopes.
 
-    switch (position){
-        case 1:
-            prefix = 'st';
-            break;
-        case 2:
-            prefix = 'nd';
-            break;
-        case 3:
-            prefix = 'rd';
-            break;
-        default:
-            prefix = 'th';
-            break;
-    }
-    console.log(`My ${position}${prefix} choice is ${item}`);
-})
+// Example:
+//
+// (function () { // the function expression is surrounded by parenthesis
+//   // variables defined here
+//   // can't be accessed outside
+// })() // the function is immediately invoked
+//
+
+// a: 1, b: 8, c: 6
+const a = 1;
+const b = 2;
+const c = 3;
+
+(function firstFunction () {
+  const b = 5;
+  const c = 6;
+
+  (function secondFunction () {
+    const b = 8;
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+
+    (function thirdFunction () {
+      const a = 7;
+      const c = 9;
+
+      (function fourthFunction () {
+        const a = 1;
+        const c = 8;
+      })()
+    })()
+  })()
+})()
 
