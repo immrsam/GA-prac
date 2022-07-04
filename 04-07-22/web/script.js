2ea051713ce1d8fc6a6c11981a40b212ec72e3f6
Sam Bradbury <sam.bradbury@hotmail.com>
2022-07-04 17:55:06 +1000

    created class 7 folder

---
+++ 04-07-22/web/script.js
@@ -0,0 +1,4 @@
+// const container = document.getElementById("container");
+const listNode = document.querySelector("#list");
+const currentStyle = getComputedStyle(listNode);
+const fontSize = currentStyle.fontSize;
