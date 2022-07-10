8227994db91c057df1272535a3b55c4bbf1ca136
Sam Bradbury <sam.bradbury@hotmail.com>
2022-07-10 18:07:38 +1000

    finished live region update task

--- liveRegionUpdate/web/script.js
+++ liveRegionUpdate/web/script.js
@@ -13,16 +13,22 @@ document.addEventListener("DOMContentLoaded", () =>{
     let manualStateIndex = 0;
     let autoStateIndex = 0;
     let autoStarted = false;
+    let loadingStatus = false;
+    let loadingCount = 0;
 
     const statusUpdateButton = document.querySelector("#statusButton");
     const statusUpdateDisplay = document.querySelector("#manualStatusUpdate");
     const autoStatusUpdateDisplay = document.querySelector("#autoStatusUpdate");
+    const loadStatusAnimation = document.querySelector("#loadStatusAnimation");
+
 
     const autoStatusNextStep = function(){
         autoStatusUpdateDisplay.textContent = states[autoStateIndex];
         autoStateIndex++;
         if(autoStateIndex < states.length - 1){
-            setTimeout(autoStatusNextStep, 1500);
+            setTimeout(autoStatusNextStep, 1000);
+        }else{
+            loadingStatus = false;
         }
     }
 
@@ -30,6 +36,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         autoStarted = true;
         if(autoStateIndex < states.length){
             console.log("auto status update started");
+            loadAnimation();
             autoStatusNextStep();
         }
 
@@ -47,7 +54,28 @@ document.addEventListener("DOMContentLoaded", () =>{
         console.log("status updating");
         manualUpdateStatus();
         if(!autoStarted){
+            loadingStatus = true;
             autoUpdateStatus();
+        }
+    }
+
+    const loadAnimation = function(){
+        if(loadingStatus){
+            let text = "";
+            if(loadingCount < 9){
+                loadingCount++;
+                for(let i = 0; i < loadingCount; i++){
+                    text += ".";
+                }
+            }else{
+                loadingCount = 0;
+            }
+            loadStatusAnimation.textContent = text;
+        }
+        if(autoStateIndex < states.length - 1){
+            setTimeout(loadAnimation, 70);
+        }else{
+            loadStatusAnimation.textContent = "";
         }
     }
 
