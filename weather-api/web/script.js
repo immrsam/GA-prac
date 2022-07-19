5ccff2502d9d273a46eef7da06990f8c42eeb9a2
Sam Bradbury <sam.bradbury@hotmail.com>
2022-07-19 22:34:32 +1000

    added api call and key, wating on activation

--- weather-api/web/script.js
+++ weather-api/web/script.js
@@ -1,1 +1,19 @@
 const container = document.getElementById("container");
+
+const apiKey = "56e731a692831d9d0a364acc80ea23b1";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather";
+const parameters = `?q=Sydney&units=metric&appid=${apiKey}`;
+
+function convertToJSObject(res) {
+  // return response converted to json
+  console.log(res.status);
+}
+
+function handleData(data) {
+    // display data converted to json in browser
+      console.log(data);
+}
+
+fetch(baseURL + parameters)
+  .then(convertToJSObject)
+  .then(handleData);
