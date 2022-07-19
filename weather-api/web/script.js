469a3b7e2e8a3704cee2ca5de751e73f9ded9044
Sam Bradbury <sam.bradbury@hotmail.com>
2022-07-19 22:48:44 +1000

    added different api gathering

--- weather-api/web/script.js
+++ weather-api/web/script.js
@@ -1,19 +1,34 @@
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
 
-const apiKey = "56e731a692831d9d0a364acc80ea23b1";
-const baseURL = "http://api.openweathermap.org/data/2.5/weather";
-const parameters = `?q=Sydney&units=metric&appid=${apiKey}`;
+// const apiKey = "56e731a692831d9d0a364acc80ea23b1";
+// const baseURL = "http://api.openweathermap.org/data/2.5/weather";
+// const parameters = `?q=Sydney&units=metric&appid=${apiKey}`;
 
-function convertToJSObject(res) {
-  // return response converted to json
-  console.log(res.status);
-}
+// function convertToJSObject(res) {
+//   // return response converted to json
+//   console.log(res);
+// }
 
-function handleData(data) {
-    // display data converted to json in browser
+// function handleData(data) {
+//     // display data converted to json in browser
+//       console.log(data);
+// }
+
+// fetch(baseURL + parameters)
+//   .then(convertToJSObject)
+//   .then(handleData);
+function weatherBalloon( cityID ) {
+    var key = '56e731a692831d9d0a364acc80ea23b1';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
       console.log(data);
-}
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
 
-fetch(baseURL + parameters)
-  .then(convertToJSObject)
-  .then(handleData);
+  window.onload = function() {
+    weatherBalloon( 6167865 );
+  }
