e2f92be94047b85d823e3df9a022b4ff51c105dd
Sam Bradbury <sam.bradbury@hotmail.com>
2022-07-28 07:49:51 +1000

    more work on afl api stats

--- afl-test/main.js
+++ afl-test/main.js
@@ -1,9 +1,12 @@
 const targetElement = document.querySelector("#app");
+const standingsElement = document.querySelector("#standings");
+const teamInfoElement = document.querySelector("#team-info");
 
 // 1. Base URL (+ Path)
 const baseUrl = "https://api.squiggle.com.au";
-const path = "/?q=standings";
-const endpoint = baseUrl + path;
+const pathStandings = "/?q=standings";
+const pathTeam = "?q=teams;team=12";
+const endpoint = baseUrl + pathStandings;
 
 // 2. Method (None - GET)
 // 3. Query String / Parameters (None)
@@ -13,43 +16,68 @@ const responseToJSObject = (res) => {
     return res.json();
 }
 
-const handleJSObject = (data) => {
+const handleJSObject = (route, data) => {
     // console.log(data);
     // debugger;
+    buildPage(route, data);
+}
 
-    targetElement.innerHTML += getStandingsHtmlTable(data);
+const buildPage = (route, data) => {
+    switch(route){
+        case "standings":
+            standingsElement.append(getStandingsHtmlTable(data));
+            break;
+        case "team":
+            teamInfoElement.append(getTeamInfo(data));
+            break;
+        default:
+            break;
+    }
+}
+
+const getTeamInfo = (data) => {
+
+    let div = document.createElement('div');
+    let html = `
+    <h1>${data.teams[0].name}</h1>
+    <p>Since ${data.teams[0].debut}</p>
+    `;
+    // debugger;
+    div.innerHTML = html;
+    return div;
 }
 
 const getStandingsHtmlTable = (data) => {
+    let div = document.createElement('div');
     let html = ``;
     html += `
     <table>
-        <tr>
-            <th>Position</th>
-            <th>Team</th>
-            <th>W</th>
-            <th>L</th>
-            <th>D</th>
-            <th>%</th>
-            <th>Pts</th>
-        </tr>`;
+    <tr>
+    <th>Position</th>
+    <th>Team</th>
+    <th>W</th>
+    <th>L</th>
+    <th>D</th>
+    <th>%</th>
+    <th>Pts</th>
+    </tr>`;
 
     data.standings.forEach(function callback(e, index) {
         html += `
         <tr>
-            <td>${index + 1}</td>
-            <td>${e.name}</td>
-            <td>${e.wins}</td>
-            <td>${e.losses}</td>
-            <td>${e.draws}</td>
-            <td>${e.percentage.toFixed(2)}</td>
-            <td>${e.pts}</td>
+        <td>${index + 1}</td>
+        <td>${e.name}</td>
+        <td>${e.wins}</td>
+        <td>${e.losses}</td>
+        <td>${e.draws}</td>
+        <td>${e.percentage.toFixed(2)}</td>
+        <td>${e.pts}</td>
         </tr>
         `;
     });
     html += `</table>`;
-
-    return html;
+    div.innerHTML = html;
+    return div;
 }
 
 const getStandingsHtmlLi = (data) => {
@@ -62,5 +90,8 @@ const getStandingsHtmlLi = (data) => {
 
     return html;
 }
+document.addEventListener("DOMContentLoaded", () =>{
 
-fetch(endpoint).then(responseToJSObject).then(handleJSObject);
+    fetch(baseUrl + pathTeam).then(responseToJSObject).then((data) => handleJSObject("team", data));
+    fetch(baseUrl + pathStandings).then(responseToJSObject).then((data) => handleJSObject("standings", data));
+});
